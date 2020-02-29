a98bba4da0e350fc1d71bf9dc07bb6f57e1c189a
oussamalouati <37141052+oussamalouati@users.noreply.github.com>
2020-02-29 14:55:42 +0100

    first commit

---
+++ maven-demo-1/src/main/java/main/mainTest.java
@@ -0,0 +1,14 @@
+package main;
+
+public class mainTest {
+
+	public mainTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("hello from the different side");
+
+	}
+
+}
